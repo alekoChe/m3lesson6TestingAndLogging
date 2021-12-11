01be24d2a0ae06122fae276ccc084ea34cb56093
alekoChe <aleko2117bis@gmail.com>
2021-12-11 13:38:46 +0300

    add detectionIs4(int[] inputArray) in ArraysTransformator.java

--- ArraysTransformator.java
+++ ArraysTransformator.java
@@ -1,4 +1,25 @@
 package ru.gdModule3lesson6.Testing;
 
 public class ArraysTransformator {
+    private int[] inputArray;
+    //private int[] outputArray;
+
+    public int[] outputArray(int[] inputArray) {
+        int[] outputArray;
+        if (!detectionIs4(inputArray)) {
+            throw new Not4Exception();
+        }
+    }
+
+    public boolean detectionIs4(int[] inputArray) {
+        /** определяем наличие четверок в массиве */
+        int counter4 = 0;
+        for (int i = 0; i < inputArray.length; i++) {
+            if (i == 4) {
+                counter4 += 1;
+                break;
+            }
+        }
+        return (counter4 > 0);
+    }
 }
