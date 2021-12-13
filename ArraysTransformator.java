29b5c053e9f833279a30ee13660716ca7736849a
alekoChe <aleko2117bis@gmail.com>
2021-12-13 12:42:22 +0300

    изменил Not4Exception, унаследовал его от MyAppException.java

--- ArraysTransformator.java
+++ ArraysTransformator.java
@@ -2,19 +2,34 @@ package ru.gdModule3lesson6.Testing;
 
 public class ArraysTransformator {
     private int[] inputArray;
-    //private int[] outputArray;
+
+    public static void main(String[] args) {
+        ArraysTransformator tr = new ArraysTransformator();
+
+        int[] arr = {1, 2, 3};
+
+        System.out.println(tr.detectionIs4(arr));
+        System.out.println(tr.detectAmountNumbersAfter4(arr));
+        System.out.println("/////////////////////////");
+        //System.out.println(tr.transformArray(arr));
+        tr.transformArray(arr);
+    }
 
     public int[] transformArray(int[] inputArray) throws Not4Exception {
         int[] resultArray;
         if (!detectionIs4(inputArray)) {
-            throw new Not4Exception();
+            System.out.println("В массиве нет четверок!");
+            throw new Not4Exception(inputArray);
         }
         else {
-            int amountNumbersBefore4 = detectedAmountNumbersBefore4(inputArray);
+            int amountNumbersBefore4 = detectAmountNumbersAfter4(inputArray);
             resultArray = new int[amountNumbersBefore4];
             for (int i = 0; i < resultArray.length; i++) {
                 resultArray[i] = inputArray[inputArray.length - resultArray.length + i];
             }
+        }
+        for (int i = 0; i < resultArray.length; i++) {
+            System.out.println(resultArray[i]);
         }
         return resultArray;
     }
@@ -23,7 +38,7 @@ public class ArraysTransformator {
         /** определяем наличие четверок в массиве */
         int counter4 = 0;
         for (int i = 0; i < inputArray.length; i++) {
-            if (i == 4) {
+            if (inputArray[i] == 4) {
                 counter4 += 1;
                 break;
             }
@@ -31,13 +46,13 @@ public class ArraysTransformator {
         return (counter4 > 0);
     }
 
-    public int detectedAmountNumbersBefore4(int[] inputArray) {
-        int counterAmountNumbersBefore4 = 0;
-        for (int i = inputArray.length - 1; i >= 0; i--) {
+    public int detectAmountNumbersAfter4(int[] inputArray) {
+        int counterAmountNumbersAfter4 = 0;
+        for (int i = inputArray.length ; i >= 0; i--) {
             if (i != 4) {
-                counterAmountNumbersBefore4 += 1;
+                counterAmountNumbersAfter4 += 1;
             } else break;
         }
-        return counterAmountNumbersBefore4;
+        return counterAmountNumbersAfter4;
     }
 }
--- Not4Exception.java
+++ Not4Exception.java
@@ -1,4 +1,10 @@
 package ru.gdModule3lesson6.Testing;
 
-public class Not4Exception extends RuntimeException{
+public class Not4Exception extends MyAppException{
+
+    private static final String MSE = "В введенном массиве %h нет числа 4!";
+
+    public Not4Exception(int[] inputArray) {
+        super(String.format(MSE, inputArray));
+    }
 }
