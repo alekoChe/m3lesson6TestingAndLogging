87d01ee20f158a62bf8c643c8d2ee9991ec27bb2
alekoChe <aleko2117bis@gmail.com>
2021-12-13 14:11:55 +0300

    исправил checkIsOnly1And4InArray(int[] inputArray)

--- ArrayOfOnesAndFours.java
+++ ArrayOfOnesAndFours.java
@@ -10,19 +10,47 @@ public class ArrayOfOnesAndFours {
 
     public static void main(String[] args) {
         ArrayOfOnesAndFours oAF = new ArrayOfOnesAndFours();
-        oAF.inputArray =new int[] {1, 4, 4, 4, 1, 1};
+        oAF.inputArray =new int[] {1, 1,4};
         System.out.println(oAF.checkIsOnly1And4InArray(oAF.inputArray));
     }
 
     public boolean checkIsOnly1And4InArray(int[] inputArray) {
-        boolean numbersAreOnly1And4 = true;
+        boolean numbersAreOnly1And4 = false;
+        boolean otherNumbers = false;
+        boolean is1 = false;
+        boolean is4 = false;
         for (int i = 0; i <  inputArray.length; i++) {
             if (inputArray[i] != 1 && inputArray[i] != 4) {
-                numbersAreOnly1And4 = false;
+                otherNumbers = true;
                 break;
             }
+            if (inputArray[i] == 1) {
+                is1 = true;
+            }
+            if (inputArray[i] == 4) {
+                is4 = true;
+            }
+        }
+        if (otherNumbers == false && is1 == true && is4 == true) {
+            numbersAreOnly1And4 = true;
         }
         //List intList = new ArrayList<>(Arrays.asList(inputArray));
         return numbersAreOnly1And4;
+    }
+    public boolean detectionIsOneInArray(int[] inputArray) {
+        boolean is1 = false;
+        List intList = new ArrayList<>(Arrays.asList(inputArray));
+        if (intList.contains(1)) {
+            is1 = true;
+        }
+        return is1;
+    }
+    public boolean detectionIsFourInArray(int[] inputArray) {
+        boolean is4 = false;
+        List intList = new ArrayList<>(Arrays.asList(inputArray));
+        if (intList.contains(4)) {
+            is4 = true;
+        }
+        return is4;
     }
 }
